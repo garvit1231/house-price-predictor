d09e58942268b91f81f19f10f938ba546741c608
garvit1231 <118107661+garvit1231@users.noreply.github.com>
2026-04-10 23:51:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -74,4 +74,4 @@ def graph_data():
         return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=10000, debug=True)
